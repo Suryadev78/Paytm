3edfe32de987c7b548173183ea0a10e155be7d44
Surya_dev <pandeysuryadev00gmail.com>
2024-08-15 00:16:26 +0530

    added few more options on navbar

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
 
 export default function Appbar({ user, lastName }) {
+  const navigate = useNavigate();
   return (
     <div className="bg-white shadow-md p-1 px-3 flex justify-between items-center">
       <div className="text-2xl font-semibold">Paytm</div>
-      <div className="flex gap-2">
+      <div className="flex gap-4">
+        <div className="flex justify-center items-center underline   hover:cursor-pointer text-xl font-semibold text-green-400">
+          <button onClick={() => navigate("/me")}>Profile</button>
+        </div>
         <div className="flex justify-center text-xl font-semibold  items-center">
-          Hello, {user} {lastName}
+          Hello{user} {lastName}
         </div>
         <div className="w-10 h-10 bg-slate-400 rounded-full flex items-center font-semibold justify-center">
           U
