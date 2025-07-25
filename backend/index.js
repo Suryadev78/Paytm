a3f8933031c06552f51e6079b77173872867faa1
suryadev78 <pandeysuryadev00@gmail.com>
2025-07-25 22:03:36 +0530

    added new cors origin

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 import cors from "cors";
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://paytm-liard.vercel.app"],
+    credentials: true
+  }));
 app.use(express.json());
 import mainrouter from "./routes/authenty.js";
 import User from "./db/db.js";
