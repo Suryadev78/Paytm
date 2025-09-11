3c93638ec05662c7cfe4c78cd9452f15eedd97a7
suryadev78 <pandeysuryadev00@gmail.com>
2025-09-12 01:55:04 +0530

    added route for homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,9 @@ import "./App.css";
 import {
   BrowserRouter,
   Route,
-  Router,
   Routes,
-  useNavigate,
 } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
@@ -17,7 +16,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
           <Route path="/signin" element={<Signin />}></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/send" element={<Send />}></Route>
